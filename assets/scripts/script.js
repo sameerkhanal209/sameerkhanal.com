e224871ae5c12b956b4a2140681a8fed8e147de1
Sameer Khanal <admin@rudrax.net>
2024-04-10 21:16:01 +0530

    Skills Hover Effect

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    let mDScreen = 768;
+    let isDesktop = window.innerWidth > mDScreen;
+
+    /* Toast */
+    function showToast(message) {
+
+        let toast = document.createElement('div');
+        toast.classList.add('toast');
+        toast.innerHTML = message;
+
+        document.body.appendChild(toast);
+
+        setTimeout(function(){
+            toast.remove();
+        }, 2700)
+
+    }
+
     /* Hamburger Menu */
 
     let everyLinkEventCreated = false;
@@ -22,8 +40,7 @@ document.addEventListener('DOMContentLoaded', function() {
     })
 
     /* Mouse Background Gradient */
-
-    if(document.querySelector('.controllable-gradient') && document.querySelector('.controllable-gradient').style.display != 'none'){
+    if(document.querySelector('.controllable-gradient') && isDesktop){
 
         const circle = document.querySelector('.controllable-gradient');
 
@@ -52,6 +69,9 @@ document.addEventListener('DOMContentLoaded', function() {
                 circle.style.top = `${maxY}px`;
             }
         });
+
+
+
     }
 
     /* Scroll to Top */
@@ -88,6 +108,66 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     }
+
+    /* Skills Section */
+    if(document.querySelector('.skills .description-holder')){
+        
+        let skills = document.querySelectorAll('.skills .content-item')
+
+        skills.forEach(skill=>{
+            
+            if(skill.querySelector('.description-holder')){
+
+                let decriptionHolder = skill.querySelector('.description-holder')
+
+                if(isDesktop){
+                    
+                    skill.addEventListener('mousemove', (event) => {
+                        decriptionHolder.style.visibility = 'visible';
+                    
+                        const mouseX = event.clientX;
+                        const mouseY = event.clientY;
+                    
+                        const decriptionHolderWidth = decriptionHolder.offsetWidth;
+                        const decriptionHolderHeight = decriptionHolder.offsetHeight;
+                    
+                        let leftPosition = mouseX;
+                        let topPosition = mouseY;
+                    
+                        // Check if there is enough space on the right
+                        if (mouseX + decriptionHolderWidth > window.innerWidth) {
+                            // If not, render from the left
+                            leftPosition = mouseX - decriptionHolderWidth;
+                        }
+                    
+                        // Check if there is enough space at the bottom
+                        if (mouseY + decriptionHolderHeight > window.innerHeight) {
+                            // If not, adjust the top position
+                            topPosition = window.innerHeight - decriptionHolderHeight;
+                        }
+                    
+                        decriptionHolder.style.top = topPosition + "px";
+                        decriptionHolder.style.left = leftPosition + "px";
+                    });
+
+                    skill.addEventListener('mouseleave', ()=>{
+                        decriptionHolder.style.visibility = 'hidden';
+                    })
+
+                } else {
+
+                    skill.addEventListener('click', ()=>{
+
+                        showToast(decriptionHolder.innerHTML)
+
+                    })
+        
+                }
+            }
+        })
+    }
+
+    //Footer
 
     document.getElementById("year").innerHTML = new Date().getFullYear();
 
